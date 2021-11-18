e72adaab787db77abd61a2485a53a367b90597c3
smallopen <steve@advmeds.com>
2021-11-18 18:24:12 +0800

    1.1.7 加入統一上傳之設備名稱（一致性） 參閱 https://docs.google.com/spreadsheets/d/1MkHaVcwgwlFYp6snqoizFm9f1lF7g6gHCVpb1XYuGus/edit#gid=636232443

--- advmeds-device-management_lib/src/main/java/com/advmeds/advmeds_device_management_lib/BleVariables.java
+++ advmeds-device-management_lib/src/main/java/com/advmeds/advmeds_device_management_lib/BleVariables.java
@@ -21,11 +21,15 @@ class BleVariables {
 
     static final String BLE_D40B_TD3261B_CONNECT = "FORA D40";
 
+    static final String BLE_D40B_TD3261B_UPLOAD = "FORA D40B";
+
     static final String BLE_P30B_TD3129B = "P30B / TD-3129B";
 
     static final String[] BLE_P30B_TD3129B_SEARCH = {"FORA P30", "FORA P30 PLUS", "TAIDOC TD3129"};
 
     static final String BLE_P30B_TD3129B_CONNECT = "FORA P30 PLUS";
+
+    static final String BLE_P30B_TD3129B_UPLOAD = "FORA P30B";
 
     static final String BLE_P80_TD3140 = "P80 / TD-3140";
 
@@ -33,11 +37,15 @@ class BleVariables {
 
     static final String BLE_P80_TD3140_CONNECT = "TAIDOC TD3140";
 
+    static final String BLE_P80_TD3140_UPLOAD = "FORA P80";
+
     static final String BLE_TD3128 = "TaiDoc TD-3128";
 
     static final String[] BLE_TD3128_SEARCH = {"TAIDOC TD3128"};
 
     static final String BLE_TD3128_CONNECT = "TAIDOC TD3128";
+
+    static final String BLE_TD3128_UPLOAD = "TaiDoc TD-3128";
 
     static final String BLE_TD2555 = "TaiDoc TD-2555";
 
@@ -45,11 +53,15 @@ class BleVariables {
 
     static final String BLE_TD2555_CONNECT = "TAIDOC TD2555";
 
+    static final String BLE_TD2555_UPLOAD = "TaiDoc TD-2555";
+
     public static final String BLE_W600_TD2560 = "FORA W600 / TD-2560";
 
-    public static final String[] BLE_W600_TD2560_SEARCH = {"FORA W600","TAIDOC TD2560"};
+    public static final String[] BLE_W600_TD2560_SEARCH = {"FORA W600", "TAIDOC TD2560"};
 
     public static final String BLE_W600_TD2560_CONNECT = "FORA W600";
+
+    public static final String BLE_W600_TD2560_UPLOAD = "FORA W600";
 
     static final String BLE_BF4030 = "BF4030";
 
@@ -57,11 +69,15 @@ class BleVariables {
 
     static final String BLE_BF4030_CONNECT = "BF4030";
 
+    static final String BLE_BF4030_UPLOAD = "BF4030";
+
     static final String BLE_IR40_TD1241 = "IR40 / TD-1241";
 
     static final String[] BLE_IR40_TD1241_SEARCH = {"FORA IR40", "TAIDOC TD1241"};
 
     static final String BLE_IR40_TD1241_CONNECT = "FORA IR40";
+
+    static final String BLE_IR40_TD1241_UPLOAD = "FORA IR40";
 
     static final String BLE_TD1261 = "TaiDoc TD-1261";
 
@@ -69,11 +85,15 @@ class BleVariables {
 
     static final String BLE_TD1261_CONNECT = "TAIDOC TD1261";
 
+    static final String BLE_TD1261_UPLOAD = "TaiDoc TD-1261";
+
     static final String BLE_TD1035 = "TaiDoc TD-1035";
 
     static final String[] BLE_TD1035_SEARCH = {"TAIDOC TD1035"};
 
     static final String BLE_TD1035_CONNECT = "TAIDOC TD1035";
+
+    static final String BLE_TD1035_UPLOAD = "TaiDoc TD-1035";
 
     static final String BLE_IR42B_TD1242 = "IR42B / TD-1242";
 
@@ -81,11 +101,15 @@ class BleVariables {
 
     static final String BLE_IR42B_TD1242_CONNECT = "FORA IR42";
 
+    static final String BLE_IR42B_TD1242_UPLOAD = "FORA IR42B";
+
     static final String BLE_O2_TD8255 = "O2 / TD-8255";
 
     static final String[] BLE_O2_TD8255_SEARCH = {"FORA O2", "TAIDOC TD8255"};
 
     static final String BLE_O2_TD8255_CONNECT = "TAIDOC TD8255";
+
+    static final String BLE_O2_TD8255_UPLOAD = "FORA O2";
 
     static final String BLE_TD8201 = "TaiDoc TD-8201";
 
@@ -93,11 +117,15 @@ class BleVariables {
 
     static final String BLE_TD8201_CONNECT = "TAIDOC TD8201";
 
+    static final String BLE_TD8201_UPLOAD = "TaiDoc TD-8201";
+
     static final String BLE_TD4257 = "TaiDoc TD-4257";
 
     static final String[] BLE_TD4257_SEARCH = {"TAIDOC TD4257"};
 
     static final String BLE_TD4257_CONNECT = "TAIDOC TD4257";
+
+    static final String BLE_TD4257_UPLOAD = "TaiDoc TD-4257";
 
     static final String BLE_GD40B_TD4272B = "FORA GD40B / TD-4272B";
 
@@ -105,11 +133,15 @@ class BleVariables {
 
     static final String BLE_GD40B_TD4272B_CONNECT = "FORA GD40";
 
+    static final String BLE_GD40B_TD4272B_UPLOAD = "FORA GD40B";
+
     static final String BLE_TD4216 = "TaiDoc TD-4216";
 
-    static final String[] BLE_TD4216_SEARCH = {"TAIDOC TD4216","TAIDOC TD4216 A7B8"};
+    static final String[] BLE_TD4216_SEARCH = {"TAIDOC TD4216", "TAIDOC TD4216 A7B8"};
 
     static final String BLE_TD4216_CONNECT = "TAIDOC TD4216";
+
+    static final String BLE_TD4216_UPLOAD = "TaiDoc TD-4216";
 
     static final String BLE_DSP_COMBO_SENSOR = "DSP Combo Sensor";
 
@@ -117,11 +149,15 @@ class BleVariables {
 
     static final String BLE_DSP_COMBO_SENSOR_CONNECT = "DSP Combo";
 
+    static final String BLE_DSP_COMBO_SENSOR_UPLOAD = "DSP Combo Sensor";
+
     static final String BLE_TD4141 = "TaiDoc TD-4141";
 
     static final String[] BLE_TD4141_SEARCH = {"TAIDOC METER"};
 
     static final String BLE_TD4141_CONNECT = "TAIDOC TD4141";
+
+    static final String BLE_TD4141_UPLOAD = "TaiDoc TD-4141";
 
     static final String BLE_TD4206 = "TaiDoc TD-4206";
 
@@ -129,12 +165,15 @@ class BleVariables {
 
     static final String BLE_TD4206_CONNECT = "TAIDOC TD4206";
 
+    static final String BLE_TD4206_UPLOAD = "TaiDoc TD-4206";
+
     static final String BLE_FORA_MD6 = "FORA MD6 / TD-4172";
 
-    static final String[] BLE_FORA_MD6_SEARCH = {"FORA MD6","TAIDOC TD4172"};
+    static final String[] BLE_FORA_MD6_SEARCH = {"FORA MD6", "TAIDOC TD4172"};
 
     static final String BLE_FORA_MD6_CONNECT = "FORA MD6";
 
+    static final String BLE_FORA_MD6_UPLOAD = "FORA MD6";
 
     static final String BLE_IMEDI_PLUS_DS101 = "Imedi Plus DS101";
 
@@ -143,6 +182,8 @@ class BleVariables {
     //    聽診器有獨立的判斷，因此理論上不需要這個東西
     static final String BLE_IMEDI_PLUS_DS101_CONNECT = "TAIDOC TD4206";
 
+    static final String BLE_IMEDI_PLUS_DS101_UPLOAD = "Imedi Plus DS101";
+
     static final String BLE_ACR3901U_S1 = "ACR3901U-S1";
 
     static final String[] BLE_ACR3901U_S1_SEARCH = {"ACR3901U-S1"};
@@ -150,5 +191,6 @@ class BleVariables {
     //    讀卡機有獨立的判斷，因此理論上不需要這個東西
     static final String BLE_ACR3901U_S1_CONNECT = "ACR3901U-S1";
 
+    static final String BLE_ACR3901U_S1_UPLOAD = "ACR3901U-S1";
 
 }
--- advmeds-device-management_lib/src/main/java/com/advmeds/advmeds_device_management_lib/BtDeviceListEnum.java
+++ advmeds-device-management_lib/src/main/java/com/advmeds/advmeds_device_management_lib/BtDeviceListEnum.java
@@ -11,100 +11,100 @@ import androidx.annotation.Keep;
 @Keep
 public enum BtDeviceListEnum {
     // Search Name，連接方式-標題，連接方式-說明，圖片
-    D40_TD3261(BLE_D40B_TD3261B, BLE_D40B_TD3261B_SEARCH, BLE_D40B_TD3261B_CONNECT,
+    D40_TD3261(BLE_D40B_TD3261B, BLE_D40B_TD3261B_SEARCH, BLE_D40B_TD3261B_CONNECT,BLE_D40B_TD3261B_UPLOAD,
             R.string.more_ble_txt_d40_and_td3261_title,
             R.string.more_ble_txt_d40_and_td3261_pressure, R.string.operation_info_taidoc_global,
             R.drawable.img_d_40_b_td_3261_b),
-    P30_TD3129(BLE_P30B_TD3129B, BLE_P30B_TD3129B_SEARCH, BLE_P30B_TD3129B_CONNECT,
+    P30_TD3129(BLE_P30B_TD3129B, BLE_P30B_TD3129B_SEARCH, BLE_P30B_TD3129B_CONNECT,BLE_P30B_TD3129B_UPLOAD,
             R.string.more_ble_txt_p30_and_td3129_title,
             R.string.more_ble_txt_p30_and_td3129
             , R.string.operation_info_taidoc_global, R.drawable.img_p_30_b_td_3129_b),
-    P80_TD3140(BLE_P80_TD3140, BLE_P80_TD3140_SEARCH, BLE_P80_TD3140_CONNECT,
+    P80_TD3140(BLE_P80_TD3140, BLE_P80_TD3140_SEARCH, BLE_P80_TD3140_CONNECT,BLE_P80_TD3140_UPLOAD,
             R.string.more_ble_txt_p80_and_td3140_title,
             R.string.more_ble_txt_p80_and_td3140, R.string.operation_info_taidoc_global,
             R.drawable.img_fora_p_80_tai_doc_td_3140),
-    TD3128(BLE_TD3128, BLE_TD3128_SEARCH, BLE_TD3128_CONNECT, R.string.more_ble_txt_td3128_title,
+    TD3128(BLE_TD3128, BLE_TD3128_SEARCH, BLE_TD3128_CONNECT,BLE_TD3128_UPLOAD, R.string.more_ble_txt_td3128_title,
             R.string.more_ble_txt_td3128, R.string.operation_info_taidoc_global,
             R.drawable.img_tai_doc_td_3128),
 
-    TD2555(BLE_TD2555, BLE_TD2555_SEARCH, BLE_TD2555_CONNECT, R.string.more_ble_txt_td2555_title,
+    TD2555(BLE_TD2555, BLE_TD2555_SEARCH, BLE_TD2555_CONNECT,BLE_TD2555_UPLOAD, R.string.more_ble_txt_td2555_title,
             R.string.more_ble_txt_td2555, R.string.operation_info_taidoc_global,
             R.drawable.img_tai_doc_td_2555),
-    W600_TD2560(BLE_W600_TD2560, BLE_W600_TD2560_SEARCH, BLE_W600_TD2560_CONNECT, R.string.more_ble_txt_w600_and_td2560_title,
+    W600_TD2560(BLE_W600_TD2560, BLE_W600_TD2560_SEARCH, BLE_W600_TD2560_CONNECT,BLE_W600_TD2560_UPLOAD, R.string.more_ble_txt_w600_and_td2560_title,
             R.string.more_ble_txt_w600_and_td2560, R.string.operation_info_taidoc_global,
             R.drawable.img_tai_doc_td_2560),
-    BF4030(BLE_BF4030, BLE_BF4030_SEARCH, BLE_BF4030_CONNECT, R.string.more_ble_txt_bf4030_title,
+    BF4030(BLE_BF4030, BLE_BF4030_SEARCH, BLE_BF4030_CONNECT, BLE_BF4030_UPLOAD, R.string.more_ble_txt_bf4030_title,
             R.string.more_ble_txt_bf4030, R.string.operation_info_bf4030,
             R.drawable.img_bf_4030),
-    IR40_TD1241(BLE_IR40_TD1241, BLE_IR40_TD1241_SEARCH, BLE_IR40_TD1241_CONNECT,
+    IR40_TD1241(BLE_IR40_TD1241, BLE_IR40_TD1241_SEARCH, BLE_IR40_TD1241_CONNECT,BLE_IR40_TD1241_UPLOAD,
             R.string.more_ble_txt_ir40_and_td1241_title,
             R.string.more_ble_txt_ir40_and_td1241, R.string.operation_info_taidoc_global,
             R.drawable.img_fora_ir_40_tai_doc_td_1241),
-    TD1261(BLE_TD1261, BLE_TD1261_SEARCH, BLE_TD1261_CONNECT, R.string.more_ble_txt_td1261_title,
+    TD1261(BLE_TD1261, BLE_TD1261_SEARCH, BLE_TD1261_CONNECT, BLE_TD1261_UPLOAD, R.string.more_ble_txt_td1261_title,
             R.string.more_ble_txt_td1261, R.string.operation_info_taidoc_global,
             R.drawable.img_tai_doc_td_1261),
-    TD1035(BLE_TD1035, BLE_TD1035_SEARCH, BLE_TD1035_CONNECT, R.string.more_ble_txt_td1035_title,
+    TD1035(BLE_TD1035, BLE_TD1035_SEARCH, BLE_TD1035_CONNECT,BLE_TD1035_UPLOAD, R.string.more_ble_txt_td1035_title,
             R.string.more_ble_txt_td1035, R.string.operation_info_taidoc_global,
             R.drawable.img_tai_doc_td_1035),
-    IR42_TD1242(BLE_IR42B_TD1242, BLE_IR42B_TD1242_SEARCH, BLE_IR42B_TD1242_CONNECT,
+    IR42_TD1242(BLE_IR42B_TD1242, BLE_IR42B_TD1242_SEARCH, BLE_IR42B_TD1242_CONNECT,BLE_IR42B_TD1242_UPLOAD,
             R.string.more_ble_txt_ir42_and_td1242_title,
             R.string.more_ble_txt_ir42_and_td1242, R.string.operation_info_taidoc_global,
             R.drawable.img_ir_42_b_td_1242),
 
-    O2_TD8255(BLE_O2_TD8255, BLE_O2_TD8255_SEARCH, BLE_O2_TD8255_CONNECT, R.string.more_ble_txt_td8255_title,
+    O2_TD8255(BLE_O2_TD8255, BLE_O2_TD8255_SEARCH, BLE_O2_TD8255_CONNECT,BLE_O2_TD8255_UPLOAD, R.string.more_ble_txt_td8255_title,
             R.string.more_ble_txt_td8255, R.string.operation_info_taidoc_global,
             R.drawable.img_fora_o_2_tai_doc_td_8255),
-    TD8201(BLE_TD8201, BLE_TD8201_SEARCH, BLE_TD8201_CONNECT, R.string.more_ble_txt_td8201_title,
+    TD8201(BLE_TD8201, BLE_TD8201_SEARCH, BLE_TD8201_CONNECT, BLE_TD8201_UPLOAD, R.string.more_ble_txt_td8201_title,
             R.string.more_ble_txt_td8201, R.string.operation_info_taidoc_global,
             R.drawable.img_tai_doc_td_8201),
 
-    TD4257(BLE_TD4257, BLE_TD4257_SEARCH, BLE_TD4257_CONNECT, R.string.more_ble_txt_td4257_title,
+    TD4257(BLE_TD4257, BLE_TD4257_SEARCH, BLE_TD4257_CONNECT,BLE_TD4257_UPLOAD, R.string.more_ble_txt_td4257_title,
             R.string.more_ble_txt_td4257, R.string.operation_info_taidoc_global,
             R.drawable.img_tai_doc_td_4257),
-    GD40_TD4272(BLE_GD40B_TD4272B, BLE_GD40B_TD4272B_SEARCH, BLE_GD40B_TD4272B_CONNECT,
+    GD40_TD4272(BLE_GD40B_TD4272B, BLE_GD40B_TD4272B_SEARCH, BLE_GD40B_TD4272B_CONNECT,BLE_GD40B_TD4272B_UPLOAD,
             R.string.more_ble_txt_gd40_and_td4272_title,
             R.string.more_ble_txt_gd40_and_td4272, R.string.operation_info_taidoc_global,
             R.drawable.img_fora_gd_40_b_tai_doc_td_4272_b),
-    D40_TD3261_SUGAR(BLE_D40B_TD3261B, BLE_D40B_TD3261B_SEARCH, BLE_D40B_TD3261B_CONNECT,
+    D40_TD3261_SUGAR(BLE_D40B_TD3261B, BLE_D40B_TD3261B_SEARCH, BLE_D40B_TD3261B_CONNECT,BLE_D40B_TD3261B_UPLOAD,
             R.string.more_ble_txt_d40_and_td3261_title,
             R.string.more_ble_txt_d40_and_td3261_sugar, R.string.operation_info_taidoc_global, R.drawable.img_d_40_b_td_3261_b),
-    TD4216_SUGAR(BLE_TD4216, BLE_TD4216_SEARCH, BLE_TD4216_CONNECT, R.string.more_ble_txt_td4216_title,
+    TD4216_SUGAR(BLE_TD4216, BLE_TD4216_SEARCH, BLE_TD4216_CONNECT,BLE_TD4216_UPLOAD, R.string.more_ble_txt_td4216_title,
             R.string.more_ble_txt_td4216_sugar, R.string.operation_info_taidoc_global, R.drawable.img_tai_doc_td_4216),
-    TD4206_SUGAR(BLE_TD4206, BLE_TD4206_SEARCH, BLE_TD4206_CONNECT, R.string.more_ble_txt_td4206_title,
+    TD4206_SUGAR(BLE_TD4206, BLE_TD4206_SEARCH, BLE_TD4206_CONNECT,  BLE_TD4206_UPLOAD, R.string.more_ble_txt_td4206_title,
             R.string.more_ble_txt_td4206_sugar, R.string.operation_info_taidoc_meter, R.drawable.img_tai_doc_td_4206),
 
-    DSP_COMBO(BLE_DSP_COMBO_SENSOR, BLE_DSP_COMBO_SENSOR_SEARCH, BLE_DSP_COMBO_SENSOR_CONNECT,
+    DSP_COMBO(BLE_DSP_COMBO_SENSOR, BLE_DSP_COMBO_SENSOR_SEARCH, BLE_DSP_COMBO_SENSOR_CONNECT,BLE_DSP_COMBO_SENSOR_UPLOAD,
             R.string.more_ble_txt_dsp_combo_title,
             R.string.more_ble_txt_dsp_combo, R.string.operation_info_dspcombo, R.drawable.img_dsp_combo_sensor),
-    MD6_TD4172_SUGAR(BLE_FORA_MD6, BLE_FORA_MD6_SEARCH, BLE_FORA_MD6_CONNECT, R.string.more_ble_txt_md6_and_td4172_title,
+    MD6_TD4172_SUGAR(BLE_FORA_MD6, BLE_FORA_MD6_SEARCH, BLE_FORA_MD6_CONNECT,  BLE_FORA_MD6_UPLOAD, R.string.more_ble_txt_md6_and_td4172_title,
             R.string.more_ble_txt_md6_and_td4172_sugar, R.string.operation_info_taidoc_global, R.drawable.img_fora_md6),
 
-    TD4141(BLE_TD4141, BLE_TD4141_SEARCH, BLE_TD4141_CONNECT, R.string.more_ble_txt_td4141_title,
+    TD4141(BLE_TD4141, BLE_TD4141_SEARCH, BLE_TD4141_CONNECT, BLE_TD4141_UPLOAD, R.string.more_ble_txt_td4141_title,
             R.string.more_ble_txt_td4141, R.string.operation_info_taidoc_meter, R.drawable.img_tai_doc_td_4141),
-    TD4206_URIC_ACID(BLE_TD4206, BLE_TD4206_SEARCH, BLE_TD4206_CONNECT, R.string.more_ble_txt_td4206_title,
+    TD4206_URIC_ACID(BLE_TD4206, BLE_TD4206_SEARCH, BLE_TD4206_CONNECT,BLE_TD4206_UPLOAD, R.string.more_ble_txt_td4206_title,
             R.string.more_ble_txt_td4206_uric_acid, R.string.operation_info_taidoc_meter, R.drawable.img_tai_doc_td_4206),
-    TD4216_URIC_ACID(BLE_TD4216, BLE_TD4216_SEARCH, BLE_TD4216_CONNECT, R.string.more_ble_txt_td4216_title,
+    TD4216_URIC_ACID(BLE_TD4216, BLE_TD4216_SEARCH, BLE_TD4216_CONNECT, BLE_TD4216_UPLOAD, R.string.more_ble_txt_td4216_title,
             R.string.more_ble_txt_td4216_uric_acid, R.string.operation_info_taidoc_meter, R.drawable.img_tai_doc_td_4216),
-    MD6_TD4172_URIC_ACID(BLE_FORA_MD6, BLE_FORA_MD6_SEARCH, BLE_FORA_MD6_CONNECT, R.string.more_ble_txt_md6_and_td4172_title,
+    MD6_TD4172_URIC_ACID(BLE_FORA_MD6, BLE_FORA_MD6_SEARCH, BLE_FORA_MD6_CONNECT,BLE_FORA_MD6_UPLOAD,R.string.more_ble_txt_md6_and_td4172_title,
             R.string.more_ble_txt_md6_and_td4172_uric_acid, R.string.operation_info_taidoc_global, R.drawable.img_fora_md6),
 
-    TD4206_KETONE(BLE_TD4206, BLE_TD4206_SEARCH, BLE_TD4206_CONNECT, R.string.more_ble_txt_td4206_title,
+    TD4206_KETONE(BLE_TD4206, BLE_TD4206_SEARCH, BLE_TD4206_CONNECT, BLE_TD4206_UPLOAD, R.string.more_ble_txt_td4206_title,
             R.string.more_ble_txt_td4206_ketone, R.string.operation_info_taidoc_global, R.drawable.img_tai_doc_td_4206),
-    TD4216_KETONE(BLE_TD4216, BLE_TD4216_SEARCH, BLE_TD4216_CONNECT, R.string.more_ble_txt_td4216_title,
+    TD4216_KETONE(BLE_TD4216, BLE_TD4216_SEARCH, BLE_TD4216_CONNECT, BLE_TD4216_UPLOAD, R.string.more_ble_txt_td4216_title,
             R.string.more_ble_txt_td4216_ketone, R.string.operation_info_taidoc_global, R.drawable.img_tai_doc_td_4216),
-    MD6_TD4172_KETONE(BLE_FORA_MD6, BLE_FORA_MD6_SEARCH, BLE_FORA_MD6_CONNECT, R.string.more_ble_txt_md6_and_td4172_title,
+    MD6_TD4172_KETONE(BLE_FORA_MD6, BLE_FORA_MD6_SEARCH, BLE_FORA_MD6_CONNECT,BLE_FORA_MD6_UPLOAD, R.string.more_ble_txt_md6_and_td4172_title,
             R.string.more_ble_txt_md6_and_td4172_ketone, R.string.operation_info_taidoc_global, R.drawable.img_fora_md6),
 
-    Imedi_Plus_DS101(BLE_IMEDI_PLUS_DS101, BLE_IMEDI_PLUS_DS101_SEARCH, BLE_IMEDI_PLUS_DS101_CONNECT,
+    Imedi_Plus_DS101(BLE_IMEDI_PLUS_DS101, BLE_IMEDI_PLUS_DS101_SEARCH, BLE_IMEDI_PLUS_DS101_CONNECT,BLE_IMEDI_PLUS_DS101_UPLOAD,
             R.string.more_ble_txt_imedi_plus_ds_101_title,
             R.string.more_ble_txt_imedi_plus_ds_101, R.string.operation_info_ds_101, R.drawable.img_imedi_ds_101),
-    ACR3901U_S1(BLE_ACR3901U_S1, BLE_ACR3901U_S1_SEARCH, BLE_ACR3901U_S1_CONNECT,
+    ACR3901U_S1(BLE_ACR3901U_S1, BLE_ACR3901U_S1_SEARCH, BLE_ACR3901U_S1_CONNECT,BLE_ACR3901U_S1_UPLOAD,
             R.string.more_ble_txt_acr_3901u_s1_title, R.string.operation_card_reader_info,
             R.string.more_ble_txt_acr_3901u_s1, R.drawable.img_acr_3901_u_s_1),
 
-    TD4216_CHOLESTEROL(BLE_TD4216, BLE_TD4216_SEARCH, BLE_TD4216_CONNECT, R.string.more_ble_txt_td4216_title,
+    TD4216_CHOLESTEROL(BLE_TD4216, BLE_TD4216_SEARCH, BLE_TD4216_CONNECT,BLE_TD4216_UPLOAD, R.string.more_ble_txt_td4216_title,
             R.string.more_ble_txt_td4216_cholesterol, R.string.operation_info_taidoc_global,R.drawable.img_tai_doc_td_4216),
-    MD6_TD4172_CHOLESTEROL(BLE_FORA_MD6, BLE_FORA_MD6_SEARCH, BLE_FORA_MD6_CONNECT, R.string.more_ble_txt_md6_and_td4172_title,
+    MD6_TD4172_CHOLESTEROL(BLE_FORA_MD6, BLE_FORA_MD6_SEARCH, BLE_FORA_MD6_CONNECT,BLE_FORA_MD6_UPLOAD, R.string.more_ble_txt_md6_and_td4172_title,
             R.string.more_ble_txt_md6_and_td4172_cholesterol, R.string.operation_info_taidoc_global, R.drawable.img_fora_md6),
 
     ;
@@ -115,6 +115,8 @@ public enum BtDeviceListEnum {
     String[] searchName;
 
     String connectName;
+
+    String uploadName;
 
     int title;
 
@@ -127,13 +129,15 @@ public enum BtDeviceListEnum {
     boolean isFastDevice = false;
 
 
-    BtDeviceListEnum(String deviceName, String[] searchName, String connectName, int title, int description,
+    BtDeviceListEnum(String deviceName, String[] searchName, String connectName, String uploadName, int title, int description,
                      int operationInfo, int pictureId) {
         this.deviceName = deviceName;
 
         this.searchName = searchName;
 
         this.connectName = connectName;
+
+        this.uploadName = uploadName;
 
         this.title = title;
 
@@ -154,6 +158,10 @@ public enum BtDeviceListEnum {
 
     public String[] getSearchName() {
         return searchName;
+    }
+
+    public String getUploadName() {
+        return uploadName;
     }
 
     public String getTitleResource(Context context) {
